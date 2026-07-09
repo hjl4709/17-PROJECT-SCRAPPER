543131777b11b536a6fcde068737f855cec59aef
hjl4709 <hjl4709@gmail.com>
2026-07-09 13:08:52 +0900

    init

---
+++ scrapper.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+keyword = "python"
+url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno=0"
+
+r = requests.get(url)
+# print(r.text)
+
+soup = BeautifulSoup(r.text, "html.parser")
+result = soup.find_all("li", class_="c_col")
